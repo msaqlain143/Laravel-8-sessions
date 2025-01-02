72742bbb61bfa8abf517c280106a6cf1ae45909c
muhammadsaqlain <muhammadsaqlain.cse@gmail.com>
2025-01-02 16:44:36 +0500

    view files changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,18 +1,42 @@
-<form action="/user" method="POST">
-    @csrf
-    <h2>User Login</h2>
-    <div class="form-group">
-        <label for="user">User Name:</label>
-        <input type="text" class="form-control" name="user" placeholder=" Enter User Name" id="name">
-    </div>
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" name="password" placeholder="Enter password" id="pwd">
-    </div>
-    <div class="form-group form-check">
-        <label class="form-check-label">
-            <input class="form-check-input" type="checkbox"> Remember me
-        </label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="/user" method="POST">
+        @csrf
+        <h2>User Login</h2>
+        <div class="form-group">
+            <label for="user">User Name:</label>
+            <input type="text" class="form-control" name="user" placeholder=" Enter User Name" id="name">
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" class="form-control" name="password" placeholder="Enter password" id="pwd">
+        </div>
+        <div class="form-group form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember me
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+
+</html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,27 @@
-<h1>Profile</h1>
-<h2>Hello {{session('user')}}</h2>
-<a href="/logout">LogOut</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Profile</h1>
+    <h2>Hello {{session('user')}}</h2>
+    <a href="/logout">LogOut</a>
+</body>
+
+</html>
